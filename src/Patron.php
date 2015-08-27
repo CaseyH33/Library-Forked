26d801558e52fb5f904f8eb2fbd475eaac910bb8
Casey Heitz & Mike Chastain <student@epicodus.com>
2015-08-27 10:02:50 -0700

    add CRUD functionality to patron class

--- src/Patron.php
+++ src/Patron.php
@@ -27,6 +27,77 @@ class Patron
         return $this->id;
     }
 
+
+    function save()
+      {
+          $GLOBALS['DB']->exec("INSERT INTO patrons_t (patron_name) VALUES ('{$this->getPatronName()}');");
+          $this->id=$GLOBALS['DB']->lastInsertId();
+      }
+
+      function update($new_name)
+      {
+          $GLOBALS['DB']->exec("UPDATE patrons_t SET patron_name = '{$new_name}' WHERE id = {$this->getId()};");
+          $this->patron_name = $new_name;
+      }
+
+      function delete()
+      {
+          $GLOBALS['DB']->exec("DELETE FROM patrons_t WHERE id = {$this->getId()};");
+      }
+
+      function addCheckout()
+      {
+          //Will add a new checkout for the patron
+          //Need to add after building checkout
+      }
+
+      function getCheckoutHistory()
+      {
+          //Will list all checkouts in this patrons history
+          //Need to add after building checkout
+      }
+
+      function getCurrentCheckouts()
+      {
+          //Will list all books currently checked out by this patron
+          //Need to add after building checkout
+      }
+
+      static function getAll()
+      {
+          $returned_patrons = $GLOBALS['DB']->query("SELECT * FROM patrons_t ORDER BY patron_name;");
+          $patrons = array();
+          foreach($returned_patrons as $patron) {
+              $patron_name = $patron['patron_name'];
+              $id = $patron['id'];
+              $new_book = new Patron($patron_name, $id);
+              array_push($patrons, $new_book);
+          }
+          return $patrons;
+      }
+
+      static function deleteAll()
+      {
+          $GLOBALS['DB']->exec("DELETE FROM patrons_t;");
+      }
+
+      static function find($search_id)
+      {
+          $found = null;
+          $patrons = Patron::getAll();
+          foreach($patrons as $patron){
+              $patron_id = $patron->getId();
+              if($patron_id == $search_id){
+                  $found = $patron;
+              }
+
+          }//end foreach
+          return $found;
+      }
+
+
+
+
 }
 
 ?>
---
+++ tests/PatronTest.php
@@ -0,0 +1,132 @@
+<?php
+
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Patron.php";
+    //require_once "src/Copy.php";
+
+    $server = 'mysql:host=localhost;dbname=library_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class PatronTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Patron::deleteAll();
+            //Checkout::deleteAll();
+        }
+
+        function testGetName()
+        {
+            $patron_name = "Randy Mclure";
+            $test_patron = new Patron($patron_name);
+
+            $result = $test_patron->getPatronName();
+
+            $this->assertEquals($patron_name, $result);
+        }
+
+        function testGetId()
+        {
+            $patron_name = "Randy Mclure";
+            $test_patron = new Book($patron_name);
+
+            $result = $test_patron->getId();
+
+            $this->assertEquals(null, $result);
+        }
+
+        function testGetAll()
+        {
+            $patron_name = "Randy Mclure";
+            $test_patron = new Patron($patron_name);
+            $test_patron->save();
+
+            $patron_name2 = "Ballface Majure";
+            $test_patron2 = new Patron($patron_name2);
+            $test_patron2->save();
+
+            $result = Patron::getAll();
+
+            $this->assertEquals([$test_patron2, $test_patron], $result);
+        }
+
+        function testSave()
+        {
+            $patron_name = "Randy Mclure";
+            $test_patron = new Patron($patron_name);
+            $test_patron->save();
+
+            $result = Patron::getAll();
+
+            $this->assertEquals($test_patron, $result[0]);
+        }
+
+        function testFind()
+        {
+            $patron_name = "Randy Mclure";
+            $test_patron = new Patron($patron_name);
+            $test_patron->save();
+
+            $patron_name2 = "Ballface Majure";
+            $test_patron2 = new Patron($patron_name2);
+            $test_patron2->save();
+
+
+            $result = Patron::find($test_patron->getId());
+
+            $this->assertEquals($result,$test_patron);
+        }
+
+        function testUpdate()
+        {
+            $patron_name = "Randy Mclure";
+            $test_patron = new Patron($patron_name);
+            $test_patron->save();
+
+            $new_name = "Neal Stephenson";
+            $test_patron->update($new_name);
+
+            $this->assertEquals($new_name,$test_patron->getPatronName());
+        }
+
+        function testDelete()
+        {
+            $patron_name = "Randy Mclure";
+            $test_patron = new Patron($patron_name);
+            $test_patron->save();
+
+            $patron_name2 = "Ballface Majure";
+            $test_patron2 = new Patron($patron_name2);
+            $test_patron2->save();
+
+            $test_patron->delete();
+
+            $this->assertEquals([$test_patron2], Patron::getAll());
+        }
+
+        function testAddCheckout()
+        {
+            //Need to add after building checkout
+        }
+
+        function testGetCheckoutHistory()
+        {
+            //Need to add after building checkout
+        }
+
+        function testGetCurrentCheckouts()
+        {
+            //Need to add after building checkout
+        }
+
+    }
+
+
+    ?>
